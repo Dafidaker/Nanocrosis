7fcb8e896627bb802f5e8f3ba923d6dd34aeff0c
Mateus Druzian <mattdruzian@gmail.com>
2023-04-04 20:46:57 +0100

    Adding the Main Menu
    Added first iteration of game's main menu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // This needs the Main Menu scene to be added BEFORE any level scenes, which must then be added in order (if / as applicable)
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
